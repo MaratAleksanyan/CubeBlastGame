7c331a5af337ce5b47e7796983a9d86a1c3a2fe5
Marat Alksanyan <marataleks@gmail.com>
2021-08-29 12:47:26 +0500

    Added favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
@@ -50,7 +51,15 @@ module.exports = {
 				collapseWhitespace: isProd
 			}
 		}),
-		new CleanWebpackPlugin()
+		new CleanWebpackPlugin(),
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, 'src/favicon.ico'),
+					to: path.resolve(__dirname, 'dist')
+				}
+			]
+		})
 	],
 	module: {
 		rules: [
